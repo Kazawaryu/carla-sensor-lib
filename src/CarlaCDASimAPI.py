b6fd037d2c93d681297a89f6860cebf442245107
west053 <167927749+west053@users.noreply.github.com>
2025-09-10 16:40:32 -0400

    Changed upper/lower fov parameter to fov for lidar sensor
    Co-Authored-By: Will Varner <willv678@gmail.com>

--- src/CarlaCDASimAPI.py
+++ src/CarlaCDASimAPI.py
@@ -195,8 +195,7 @@ def generate_lidar_bp(blueprint_library, carla_sensor_config, type= None):
         lidar_bp = blueprint_library.find("sensor.lidar.ray_cast_semantic")
     else:
         lidar_bp = blueprint_library.filter(type)[0]
-    lidar_bp.set_attribute("upper_fov", str(carla_sensor_config["upper_fov"]))
-    lidar_bp.set_attribute("lower_fov", str(carla_sensor_config["lower_fov"]))
+    lidar_bp.set_attribute("fov", str(carla_sensor_config["fov"]))
     lidar_bp.set_attribute("channels", str(carla_sensor_config["channels"]))
     lidar_bp.set_attribute("range", str(carla_sensor_config["range"]))
     lidar_bp.set_attribute("rotation_frequency", str(1.0 / carla_sensor_config["rotation_period"]))
