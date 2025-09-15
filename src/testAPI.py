8e989c1e09f64dc9823d9b1acd826018b831f165
west053 <rwf88570@uga.edu>
2025-09-15 13:24:21 -0400

    Added noise model config handling to test script

--- src/testAPI.py
+++ src/testAPI.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from CarlaCDASimAPI import CarlaCDASimAPI
 
-def _run_test(client, carla_config, simulated_sensor_config):
+def _run_test(client, carla_config, simulated_sensor_config, noise_config):
     """
     Runs the core test logic.
     """
@@ -35,7 +35,7 @@ def _run_test(client, carla_config, simulated_sensor_config):
     simulated_sensor = api.create_simulated_semantic_lidar_sensor(
         simulated_sensor_config=simulated_sensor_config,
         carla_sensor_config=sensor_config["attributes"],
-        noise_model_config={"noise_model_name": "identity"},
+        noise_model_config=noise_config,
         detection_cycle_delay_seconds=0.1,
         infrastructure_id="test_infrastructure",
         sensor_id=sensor_config["id"],
@@ -96,13 +96,15 @@ def main(args):
         
         with open(args.file) as f:
             carla_config = json.load(f)
-        with open(args.sensor_config_file) as f:
+        with open(args.sensor_config) as f:
             simulated_sensor_config = yaml.safe_load(f)
+        with open(args.noise_config) as f:
+            noise_config = yaml.safe_load(f)
 
         print('Loaded configuration from stack.json')
 
         # The _run_test function now returns the spawned actors
-        vehicle, simulated_sensor = _run_test(client, carla_config, simulated_sensor_config)
+        vehicle, simulated_sensor = _run_test(client, carla_config, simulated_sensor_config, noise_config)
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
@@ -125,7 +127,8 @@ if __name__ == '__main__':
     argparser.add_argument('--host', metavar='H', default='localhost', help='IP of the host CARLA Simulator (default: localhost)')
     argparser.add_argument('--port', metavar='P', default=2000, type=int, help='TCP port of CARLA Simulator (default: 2000)')
     argparser.add_argument('-f1', '--file', default='stack.json', help='Configuration file to be used (default: stack.json)')
-    argparser.add_argument('-f2', '--sensor_config_file', default='../config/simulated_sensor_config.yaml', help='Simulated sensor configuration file to be used (default: config/simulated_sensor_config.yaml)')
+    argparser.add_argument('-f2', '--sensor_config', default='../config/simulated_sensor_config.yaml', help='Simulated sensor configuration file to be used (default: config/simulated_sensor_config.yaml)')
+    argparser.add_argument('-f3', '--noise_config', default='../config/noise_model_config.yaml', help='Noise model config .yaml file')
     argparser.add_argument('-v', '--verbose', action='store_true', dest='debug', help='print debug information')
 
     args = argparser.parse_args()
