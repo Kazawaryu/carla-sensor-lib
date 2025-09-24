0d5124b8bd6dbf397a7408327c89440f682e0e5a
Will Varner <willv678@gmail.com>
2025-09-24 10:42:53 -0400

    lidar FOV changes

--- src/CarlaCDASimAPI.py
+++ src/CarlaCDASimAPI.py
@@ -136,7 +136,6 @@ class CarlaCDASimAPI:
         # Register the sensor
         self.__infrastructure_sensors[(infrastructure_id, sensor_id)] = simulated_sensor
 
-
         # Adding corresponding dummy lidar solely for visualization in Carla Viz
         # because semantic lidar sensor is not visualizable at the moment
         # https://github.com/usdot-fhwa-stol/carma-utils/issues/180
@@ -197,10 +196,19 @@ def generate_lidar_bp(blueprint_library, carla_sensor_config, type= None):
         lidar_bp = blueprint_library.filter(type)[0]
     print("attempting to set bp attributes")
     print(carla_sensor_config) # debug print
-    lidar_bp.set_attribute("lower_fov", str(carla_sensor_config["lower_fov"]))
-    lidar_bp.set_attribute("upper_fov", str(carla_sensor_config["upper_fov"]))
+    
+    # Check if 'horizontal_fov' is provided in the configuration, otherwise use a default.
+    if "horizontal_fov" in carla_sensor_config:
+        lidar_bp.set_attribute("horizontal_fov", str(carla_sensor_config["horizontal_fov"]))
+    else:
+        # Since older versions used upper/lower, we can set a default for 360-degree coverage
+        # or calculate a horizontal FOV based on existing parameters if a formula is available.
+        # For simplicity, we'll set a reasonable default.
+        lidar_bp.set_attribute("horizontal_fov", "360.0")
+
     lidar_bp.set_attribute("channels", str(carla_sensor_config["channels"]))
-    lidar_bp.set_attribute("range", str(carla_sensor_config["range"]))
+    lidar_bp.set_attribute("range", str(carla_sensor_config["range"])) # Note: range is now in meters
     lidar_bp.set_attribute("rotation_frequency", str(1.0 / carla_sensor_config["rotation_period"]))
     lidar_bp.set_attribute("points_per_second", str(carla_sensor_config["points_per_second"]))
+    
     return lidar_bp
